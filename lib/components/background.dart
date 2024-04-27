888ac6d1624f6e9d64fe9af0e6c5584835f93484
Calculator <satyamdevyaduvanshi@gmail.com>
2024-04-27 12:10:18 +0530

    first change

---
+++ lib/components/background.dart
@@ -0,0 +1,20 @@
+import 'dart:async';
+
+import 'package:flame/components.dart';
+import 'package:flame/flame.dart';
+import 'package:flappybird/game/assets.dart';
+import 'package:flappybird/game/flappy_bird_game.dart';
+
+import '../game/assets.dart';
+import '../game/flappy_bird_game.dart';
+
+class Background extends SpriteComponent with HasGameRef<FlappyBirdGame> {
+  Background();
+
+  @override
+  Future<void> onLoad() async {
+    final background = await Flame.images.load(Assets.backgorund);
+    size = gameRef.size;
+    sprite = Sprite(background);
+  }
+}
